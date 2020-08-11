4742f2c637f40881b0cece14c5eb6a4bf3d3bc8b
Phusan Moonmuang <phumoonlight@hotmail.com>
2020-08-11 17:14:55 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Paper from '@material-ui/core/Paper'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
 import MessageCard from '../src/components/MessageCard'
+import Credit from '../src/components/Credit'
 import useSocket from '../src/libs/useSocket'
 import useSocketMessage from '../src/libs/useSocketMessage'
 import css from '../src/styles/pages.module.css'
@@ -16,40 +17,44 @@ const Index = () => {
     emitMessage({ sender: username.value, message: message.value })
     message.value = ''
   }
+  if (!socket) return <div className="page-load">Loading...</div>
   return (
     <div className={css.root}>
-      <Paper className={css.formContainer}>
-        <form onSubmit={handleSubmit} className={css.form}>
-          <div className={css.formBlock}>
-            <TextField
-              variant="filled"
-              label="Username"
-              name="username"
-              size="small"
-              className={css.formInput}
-            />
-          </div>
-          <div className={css.formBlock}>
-            <TextField
-              variant="filled"
-              label="What's on your mind"
-              name="message"
-              size="medium"
-              className={css.formInput}
-            />
-          </div>
-          <div className={css.formBlock}>
-            <Button
-              color="primary"
-              variant="contained"
-              type="submit"
-              className={css.formInput}
-            >
-              POST
-            </Button>
-          </div>
-        </form>
-      </Paper>
+      <div className={css.formlayout}>
+        <div className={css['page-title']}>Simple Web Chat - socket.io</div>
+        <Paper className={css.formContainer}>
+          <form onSubmit={handleSubmit} className={css.form}>
+            <div className={css.formBlock}>
+              <TextField
+                variant="outlined"
+                label="Username"
+                name="username"
+                size="small"
+                className={css.formInput}
+              />
+            </div>
+            <div className={css.formBlock}>
+              <TextField
+                variant="outlined"
+                label="Message"
+                name="message"
+                size="medium"
+                className={css.formInput}
+              />
+            </div>
+            <div className={css.formBlock}>
+              <Button
+                color="primary"
+                variant="contained"
+                type="submit"
+                className={css.formInput}
+              >
+                POST
+              </Button>
+            </div>
+          </form>
+        </Paper>
+      </div>
       <div className={css.message}>
         {messages.map((log) => (
           <MessageCard
@@ -60,10 +65,7 @@ const Index = () => {
           />
         ))}
       </div>
-      <Paper className={css.socketId}>
-        {'Your ID : '}
-        {socket && socket.id}
-      </Paper>
+      <Credit />
     </div>
   )
 }
