eefe3f482d9c8b1fe3c6db1783c50338b36c2194
Phusan Moonmuang <phumoonlight@hotmail.com>
2020-08-11 17:15:01 +0700

    Create Credit.js

---
+++ src/components/Credit.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import css from './Credit.module.css'
+
+const Credit = () => (
+  <div className={css.root}>
+    {'< > by '}
+    <a href="https://github.com/phumoonlight/" target="_blank" rel="noopener noreferrer">@phumoonlight</a>
+  </div>
+)
+
+export default Credit
